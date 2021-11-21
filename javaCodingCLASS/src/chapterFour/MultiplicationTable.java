2b07eb401978014b4a418abe51a9d90fa2471995
Johnycrown <oladejijohn60@gmail.com>
2021-11-21 18:45:27 +0100

    ...

---
+++ javaCodingCLASS/src/chapterFour/MultiplicationTable.java
@@ -0,0 +1,34 @@
+package chapterFour;
+
+public class MultiplicationTable {
+    public static void main(String [] args ){
+        int counter = 1;
+
+
+        for( ; counter <= 20; counter++){
+
+            for (int counterb = 1; counterb <= 20; counterb++){
+
+                System.out.print( counter * counterb + "\t\t"  );
+
+
+            }
+
+
+            System.out.println();
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
