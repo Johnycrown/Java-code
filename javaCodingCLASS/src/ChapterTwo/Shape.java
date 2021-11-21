94599a3acebd9daf8780d4137ff49e0627be81d5
Johnycrown <oladejijohn60@gmail.com>
2021-11-21 18:51:17 +0100

    ...

---
+++ javaCodingCLASS/src/ChapterTwo/Shape.java
@@ -0,0 +1,13 @@
+//Exercise 2.18
+public class Shape {
+public static void main(String[] args){
+	System.out.println("*****\t ***\t  *  \t   *");
+	System.out.println("*   *\t*   *\t *** \t *   * ");
+	System.out.println("*   *\t*   *\t*****\t*     *");
+	System.out.println("*   *\t*   *\t  *  \t *   *");
+	System.out.println("*****\t *** \t  *  \t   *");
+
+
+}
+
+}
