cdce60aa892b92b9d2b6b9822af86aa700ad47a6
Johnycrown <oladejijohn60@gmail.com>
2021-11-21 18:43:13 +0100

    ...

---
+++ javaCodingCLASS/src/chapterEight/TicTacToe/MainClass.java
@@ -0,0 +1,11 @@
+package chapterEight.TicTacToe;
+
+public class MainClass {
+    public static void main(String[] args) {
+        GameClass allGame = new GameClass();
+        allGame.StartGame();
+    }
+
+}
+
+
